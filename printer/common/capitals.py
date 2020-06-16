83ae986d63e40afca0752416e149739b973cc385
Tian <junjie.tian.46@gmail.com>
2020-06-16 10:31:34 +0800

    add capitals

---
+++ printer/common/capitals.py
@@ -0,0 +1,165 @@
+# capital data
+
+# data ((0/1, X, Y), ....)
+# 0 -> pen up, 1 -> pen down
+# X -> x distance move
+# Y -> y distance move
+
+
+def A():
+    data = ((0, 0, 3),
+            (1, 1.5, -3),
+            (1, 1.5, 3),
+            (1, -0.75, -1.5),
+            (1, -1.5, 0),
+            (0, -0.75, -1.5))
+    return data
+
+
+def B():
+    pass
+
+
+def C():
+    data = ((0, 3, 0),
+            (1, -2, 0),
+            (1, -1, 1),
+            (1, 0, 1),
+            (1, 1, 1),
+            (1, 2, 0),
+            (0, 3, 3))
+    return data
+
+
+def D():
+    pass
+
+
+def E():
+    pass
+
+
+def F():
+    pass
+
+
+def G():
+    pass
+
+
+def H():
+    pass
+
+
+
+def I():
+    pass
+
+
+def J():
+    data = ((1, 3, 0),
+            (0, -1.5, 0),
+            (1, 0, 2.25),
+            (1, -0.75, 0.75),
+            (1, -0.75, -0.75),
+            (0, 0, -2.25))
+    return data
+
+
+def K():
+    data = ((1, 0, 3),
+            (0, 1.5, 0),
+            (1, -1.5, -1.5),
+            (1, 1.5, 1.5),
+            (0, -1.5, 0))
+    return data
+
+
+def L():
+    pass
+
+
+def M():
+    pass
+
+
+def N():
+    pass
+
+
+
+def O():
+    pass
+
+
+def P():
+    pass
+
+
+def Q():
+    pass
+
+
+def R():
+    pass
+
+
+def T():
+    pass
+
+
+def U():
+    pass
+
+
+def V():
+    pass
+
+
+def W():
+    pass
+
+
+def X():
+    pass
+
+
+def Y():
+    pass
+
+
+def Z():
+    pass
+
+
+capitals = {
+    "A": A(),
+    "B": B(),
+    "C": C(),
+    "D": D(),
+    "E": E(),
+    "F": F(),
+    "G": G(),
+    "H": H(),
+    "I": I(),
+    "J": J(),
+    "K": K(),
+    "L": L(),
+    "M": M(),
+    "N": N(),
+    "O": O(),
+    "P": P(),
+    "Q": Q(),
+    "R": R(),
+    "S": S(),
+    "T": T(),
+    "U": U(),
+    "V": V(),
+    "W": W(),
+    "X": X(),
+    "Y": Y(),
+    "Z": Z(),
+}
+
+
+
