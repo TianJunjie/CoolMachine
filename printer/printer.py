9875d9d2e4eca565387f14338f152886bdf35cb3
Tian Junjie <junjie.tian.46@gmail.com>
2020-06-17 23:44:24 +0800

    fix

--- printer/printer.py
+++ printer/printer.py
@@ -15,6 +15,11 @@ class boostprinter(object):
     BASE_TIMED = 1.0
     X_BASE_SPEED = 0.175
     Y_BASE_SPEED = 0.07
+
+    # base on angle moved
+    BASE_SPEED_ANGLE = 0.1
+    BASE_ANGLE = 180
+
     _pen_down = False
 
     def __init__(self):
@@ -53,11 +58,11 @@ class boostprinter(object):
             direction_x = 1
             if line_xy[1] < 0:
                 direction_x = -1
-            time_x = line_xy[1] * self.BASE_TIMED / 3
+            time_x = line_xy[1] * self.BASE_TIMED / 2
             logger.error("line X time {} speed {}".format(abs(time_x), self.X_BASE_SPEED * direction_x))
             self.hub.motor_A.timed(abs(time_x), self.X_BASE_SPEED * direction_x)
         elif line_xy[1] == 0 and line_xy[2] != 0:
-            time_y = line_xy[2] * self.BASE_TIMED / 3
+            time_y = line_xy[2] * self.BASE_TIMED / 2
             direction_y = 1
             if line_xy[2] < 0:
                 direction_y = -1
@@ -73,26 +78,61 @@ class boostprinter(object):
             ratio = 1
             speed_x = self.X_BASE_SPEED
             speed_y = self.Y_BASE_SPEED
-            time_xy = 0
-            if abs(line_xy[1]) > abs(line_xy[2]):
-                ratio = (abs(line_xy[1]) / abs(line_xy[2]))
-                speed_x = self.X_BASE_SPEED * ratio * direction_x
-                speed_y = self.Y_BASE_SPEED * direction_y
-                time_xy = line_xy[2] * self.BASE_TIMED / 3
-            else:
-                ratio = (abs(line_xy[2]) / abs(line_xy[1]))
-                speed_y = self.Y_BASE_SPEED * ratio * direction_y
-                speed_x = self.X_BASE_SPEED * direction_x
-                time_xy = line_xy[1] * self.BASE_TIMED / 3
+            time_xy = line_xy[1] * self.BASE_TIMED / 2
+            speed_x = self.X_BASE_SPEED * direction_x
+            speed_y = self.Y_BASE_SPEED * direction_y
+
             logger.error("line XY time {} speedx {} speedy {}".format(abs(time_xy), speed_x, speed_y))
             self.hub.motor_AB.timed(abs(time_xy), speed_x, speed_y)
+
+    def line_angle(self, line_xy):
+        if line_xy[0] == 1:
+            self.down_pen()
+        elif line_xy[0] == 0:
+            self.up_pen()
+        else:
+            pass
+
+        logger.error("line {}".format(line_xy))
+        if line_xy[1] == 0 and line_xy[2] == 0:
+            pass
+        elif line_xy[1] != 0 and line_xy[2] == 0:
+            direction_x = 1
+            if line_xy[1] < 0:
+                direction_x = -1
+            time_x = line_xy[1] * self.BASE_ANGLE
+            logger.error("line X time {} speed {}".format(abs(time_x), self.X_BASE_SPEED * direction_x))
+            self.hub.motor_A.angled(abs(time_x), self.BASE_SPEED_ANGLE * direction_x)
+        elif line_xy[1] == 0 and line_xy[2] != 0:
+            direction_y = 1
+            if line_xy[2] < 0:
+                direction_y = -1
+            time_y = line_xy[2] * self.BASE_ANGLE
+            logger.error("line Y time {} speed {}".format(abs(time_y), self.Y_BASE_SPEED * direction_y))
+            self.hub.motor_B.angled(abs(time_y), self.BASE_SPEED_ANGLE * direction_y)
+        else:
+            direction_x = 1
+            direction_y = 1
+            if line_xy[1] < 0:
+                direction_x = -1
+            if line_xy[2] < 0:
+                direction_y = -1
+            ratio = 1
+            speed_x = self.X_BASE_SPEED
+            speed_y = self.Y_BASE_SPEED
+            time_xy = line_xy[1] * self.BASE_ANGLE
+            speed_x = self.BASE_SPEED_ANGLE * direction_x
+            speed_y = self.BASE_SPEED_ANGLE * direction_y
+
+            logger.error("line XY time {} speedx {} speedy {}".format(abs(time_xy), speed_x, speed_y))
+            self.hub.motor_AB.angled(abs(time_xy), speed_x, speed_y)
 
 
     def print_cap(self, cap):
         data = capitals[cap]()
         if data is not None:
             for lineXY in data:
-                self.line(lineXY)
+                self.line_angle(lineXY)
 
     def _get_motor_for_pen(self):
         if isinstance(self.hub.port_C, EncodedMotor):
@@ -107,7 +147,11 @@ class boostprinter(object):
         printer.test_x_move_back()
         printer.test_y_move_back()
 
-
+    def fresh_xy_angle(self):
+        printer.test_x_move_angle()
+        printer.test_y_move_angle()
+        printer.test_x_move_back_angle()
+        printer.test_y_move_back_angle()
 
     def test_x_move(self):
         self.hub.motor_A.timed(self.BASE_TIMED, self.X_BASE_SPEED)
@@ -120,6 +164,18 @@ class boostprinter(object):
 
     def test_y_move_back(self):
         self.hub.motor_B.timed(self.BASE_TIMED, -self.Y_BASE_SPEED)
+
+    def test_x_move_angle(self):
+        self.hub.motor_A.angled(self.BASE_ANGLE, self.BASE_SPEED_ANGLE)
+
+    def test_y_move_angle(self):
+        self.hub.motor_B.angled(self.BASE_ANGLE, self.BASE_SPEED_ANGLE)
+
+    def test_x_move_back_angle(self):
+        self.hub.motor_A.angled(self.BASE_ANGLE, -self.BASE_SPEED_ANGLE)
+
+    def test_y_move_back_angle(self):
+        self.hub.motor_B.angled(self.BASE_ANGLE, -self.BASE_SPEED_ANGLE)
 
     def test_xy_move_dif(self):
         self.hub.motor_AB.timed(self.BASE_TIMED, self.X_BASE_SPEED, self.Y_BASE_SPEED)
@@ -138,10 +194,11 @@ if __name__ == '__main__':
 
     try:
         #printer.down_pen()
-        printer.fresh_xy()
+        printer.fresh_xy_angle()
+        #printer.fresh_xy()
          #printer.test_xy_move_dif()
          #printer.test_xy_move_back_dif()
-        printer.print_cap("A")
+        printer.print_cap("F")
     finally:
         printer.up_pen()
 
